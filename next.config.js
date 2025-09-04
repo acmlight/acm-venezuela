e82767a813827e7f48000f06c5b7e05fcfcf4c7c
Juan Murga <juanmurga97@gmail.com>
2025-09-04 22:03:00 +0200

    Remove swcMinify from Next.js configuration

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
